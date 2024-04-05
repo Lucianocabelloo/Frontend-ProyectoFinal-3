84f10b59343b8ffabe21d245e18f6c02af19688f
lucianocabelloo <you@example.com>
2024-04-05 11:24:00 -0300

    Se creo: Componentes Footer y Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import Footer from './components/common/Footer'
+import Navbar from './components/common/Navbar'
 
 function App() {
 
   return (
     <>
-        <h1>Proyecto Limpiado</h1>
+        <h1>Proyecto Limpio</h1>
+        <Navbar/>
+        <Footer/>
     </>
   )
 }
