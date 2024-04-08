bfcbac19c71af023fc5eab0b88a47968a8e87202
lucianocabelloo <you@example.com>
2024-04-08 14:18:04 -0300

    Se agrego: Img junto con su css y su logica para el mapeo de imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import "bootstrap-icons/font/bootstrap-icons.min.css";
 import "./App.css";
 import Footer from "./components/common/Footer";
 import Menu from "./components/common/Menu";
-import Administrador from "./components/pages/Administrador";
+import Gallery from "./components/pages/gallery/Gallery";
 
 function App() {
   return (
     <>
       <Menu />
-      <Administrador></Administrador>
+      <Gallery/>
       <Footer />
     </>
   );
--- src/components/pages/gallery/Gallery.jsx
+++ src/components/pages/gallery/Gallery.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Container } from 'react-bootstrap'
+import  img1 from "../../../assets/img/room1.jpg"
+import  img2 from "../../../assets/img/room2.jpg"
+import  img3 from "../../../assets/img/room3.jpg"
+import  img4 from "../../../assets/img/room4.jpg"
+import  img5 from "../../../assets/img/room5.jpg"
+import  img6 from "../../../assets/img/room6.jpg"
+import "./gallery.css"
 
 const Gallery = () => {
+
+    const data = [
+        {
+        id: 1,
+        imgSrc: img1
+    },
+        {
+        id: 2,
+        imgSrc: img2
+    },
+    {
+    id: 5,
+    imgSrc: img5
+},
+        {
+        id: 3,
+        imgSrc: img3
+    },
+        {
+        id: 4,
+        imgSrc: img4
+    },
+        {
+        id: 6,
+        imgSrc: img6
+    }
+]
+const [model, setModel] = useState(false)
+const [tempImgSrc, setTempImgSrc] = useState("")
+const getImg = (imgSrc)=> {
+    setTempImgSrc(imgSrc)
+    setModel(true)
+}
+
   return (
-    <div>Gallery</div>
+    <Container fluid className='my-5'>
+        <div className={model? "model open": "model"}>
+        <img src={tempImgSrc}></img>
+            <svg onClick={() => setModel(false)} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+  <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+</svg>
+        </div>
+        <h2>Gallery</h2>
+        <div className='gallery'>
+            {
+                data.map((item, index) => {
+                    return(
+                        <div className='pics' key={index} onClick={()=> getImg(item.imgSrc) }>
+                            <img src={item.imgSrc} alt={`Room ${item.id}`} style={{width: '100%'}} />
+                        </div>
+                    )
+                })
+            }
+        </div>
+    </Container>
   )
 }
 
