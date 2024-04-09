a9e2cda66d47d2ae14dec8f1126662130114c916
Martín Cristian Cornejo <martincckd130@gmail.com>
2024-04-09 10:59:33 -0300

    Se agregaron las fuentes de google fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.min.css";
-import "./App.css";
 import Footer from "./components/common/Footer";
 import Menu from "./components/common/Menu";
 import Gallery from "./components/pages/gallery/Gallery";
 import Home from "./components/pages/Home";
+import "./App.css";
 
 function App() {
   return (
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ import { Container } from "react-bootstrap";
 const Banner = () => {
     return (
         <div className="banner-container">
-
+            <h1>Hola</h1>
         </div>
     );
 };
