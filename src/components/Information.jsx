94c62a0ba4ea48ed59dccb50dea01600e227e9f0
Martín Cristian Cornejo <martincckd130@gmail.com>
2024-04-10 01:12:44 -0300

    Se realizo el diseño de las cards donde mostramos los diferentes servicios que ofrece el hotel

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -1,7 +1,11 @@
-import { Row, Col, Container } from "react-bootstrap";
+import { Row, Col, Container, Card } from "react-bootstrap";
 import room1 from "../assets/img/room1.jpg";
 import room2 from "../assets/img/room2.jpg";
 import room3 from "../assets/img/room3.jpg";
+import pool from "../assets/img/pool.jpg";
+import gym from "../assets/img/gym.jpg";
+import spa from "../assets/img/spa.jpg";
+import restaurant from "../assets/img/restaurant.jpg";
 
 const Information = () => {
   return (
@@ -23,26 +27,124 @@ const Information = () => {
           <Col md={4}>
             <img
               src={room1}
-              className="img-customized"
+              className="img-room"
               alt="Imagen de la habitacion"
             />
           </Col>
           <Col md={4}>
             <img
               src={room2}
-              className=" img-customized"
+              className=" img-room"
               alt="Imagen de la habitacion"
             />
           </Col>
           <Col md={4}>
             <img
               src={room3}
-              className="img-customized"
+              className="img-room"
               alt="Imagen de la habitacion"
             />
           </Col>
-          <Col md={12} className="text-center mt-4">
-          <button className="btn-customized-2">Ver Habitaciones</button>
+          <Col md={12} className="text-center my-4 ">
+            <button className="btn-customized-2">Ver Habitaciones</button>
+          </Col>
+        </Row>
+      </section>
+      <hr className="text-bg-color" />
+      <section className="my-5">
+        <h3 className="fs-1 text-center txt-bg-color pb-3">
+          Sumérgete en el Mundo del Lujo: Experiencias Excepcionales en Nuestro
+          Hotel
+        </h3>
+        <p className="text-center txt-bg-color">
+          Descubre un oasis de elegancia y confort, donde cada detalle está
+          diseñado para superar tus expectativas y brindarte una estancia
+          inolvidable.
+        </p>
+        <Row className="justify-content-center align-items-center my-5 gap-5">
+          <Col md={5}>
+            <div className="card-customized bg-color p-1">
+              <div className="img-container">
+                <img
+                  src={pool}
+                  className="img-customized"
+                  alt="Imagen de la piscina del hotel"
+                />
+              </div>
+              <div className="card-body p-2">
+                <h4 className="kaushan-script txt-details-color fs-2 pt-2">
+                  Piscina
+                </h4>
+                <hr className="text-warning my-2"/>
+                <p className="txt-light-customized">
+                  Fresco oasis de relajación bajo el sol radiante: ¡tu escapada
+                  perfecta te espera junto a nuestra piscina de ensueño!
+                </p>
+              </div>
+            </div>
+          </Col>
+          <Col md={5}>
+            <div className="card-customized bg-color p-1">
+              <div className="img-container">
+                <img
+                  src={pool}
+                  className="img-customized"
+                  alt="Imagen de la piscina del hotel"
+                />
+              </div>
+              <div className="card-body p-2">
+                <h4 className="kaushan-script txt-details-color fs-2 pt-2">
+                  Piscina
+                </h4>
+                <hr className="text-warning my-2"/>
+                <p className="txt-light-customized">
+                  Fresco oasis de relajación bajo el sol radiante: ¡tu escapada
+                  perfecta te espera junto a nuestra piscina de ensueño!
+                </p>
+              </div>
+            </div>
+          </Col>
+          <Col md={5}>
+            <div className="card-customized bg-color p-1">
+              <div className="img-container">
+                <img
+                  src={pool}
+                  className="img-customized"
+                  alt="Imagen de la piscina del hotel"
+                />
+              </div>
+              <div className="card-body p-2">
+                <h4 className="kaushan-script txt-details-color fs-2 pt-2">
+                  Piscina
+                </h4>
+                <hr className="text-warning my-2"/>
+                <p className="txt-light-customized">
+                  Fresco oasis de relajación bajo el sol radiante: ¡tu escapada
+                  perfecta te espera junto a nuestra piscina de ensueño!
+                </p>
+              </div>
+            </div>
+          </Col>
+          <Col md={5}>
+            <div className="card-customized bg-color p-1">
+              <div className="img-container">
+                <img
+                  src={pool}
+                  className="img-customized"
+                  alt="Imagen de la piscina del hotel"
+                />
+              </div>
+              <div className="card-body p-2">
+                <h4 className="kaushan-script txt-details-color fs-2 pt-2">
+                  Piscina
+                </h4>
+                <hr className="text-warning my-2"/>
+                <p className="txt-light-customized">
+                  Fresco oasis de relajación bajo el sol radiante: ¡tu escapada
+                  perfecta te espera junto a nuestra piscina de ensueño!
+                </p>
+              </div>
+            </div>
           </Col>
         </Row>
       </section>
