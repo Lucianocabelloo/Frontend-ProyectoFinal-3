5e6c8c0db12fa55f3ecbfe04acc6dd227199cf67
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2024-04-11 12:14:17 -0300

    Se agrego: las validaciones al formulario con react hook form

--- src/components/pages/users/UserForm.jsx
+++ src/components/pages/users/UserForm.jsx
@@ -15,29 +15,87 @@ const UserForm = ({ editar, titulo }) => {
     if (editar) {
       console.log("Aca se edita");
     } else {
-      console.log(room);
+      console.log(user);
     }
   };
 
   return (
     <Container className="my-4 mainContainer">
       <h1 className="mb-3">{titulo} Usuario</h1>
-      <hr className="hrRoom"/>
+      <hr className="hrRoom" />
       <Form onSubmit={handleSubmit(onSubmit)}>
         <Form.Group className="mb-3 text-light" controlId="nombreCompleto">
           <Form.Label>Nombre Completo:*</Form.Label>
-          <Form.Control type="text" placeholder="Ej. Juan Perez" />
-          <Form.Text className="text-danger">Error</Form.Text>
+          <Form.Control
+            type="text"
+            placeholder="Ej. Juan Perez"
+            {...register("nombreCompleto", {
+              required: "El nombre completo es obligatorio",
+              minLength: {
+                value: 3,
+                message:
+                  "El nombre completo debe tener como minimo 3 caracteres",
+              },
+              maxLength: {
+                value: 80,
+                message:
+                  "El nombre completo debe tener como máximo 80 caracteres",
+              },
+            })}
+          />
+          <Form.Text className="text-danger">
+            {errors.nombreCompleto?.message}
+          </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3 text-light" controlId="email">
           <Form.Label>Email:*</Form.Label>
-          <Form.Control type="email" placeholder="usuario@usuario.com" />
-          <Form.Text className="text-danger">Error</Form.Text>
+          <Form.Control
+            type="email"
+            placeholder="usuario@usuario.com"
+            {...register("email", {
+              required: "El email es obligatorio",
+              minLength: {
+                value: 10,
+                message: "El email debe tener 10 caracteres como minimo",
+              },
+              maxLength: {
+                value: 340,
+                message: "El email debe tener como máximo 340 caracteres",
+              },
+              pattern: {
+                value:
+                  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                message: "Debes ingresar un formato valido de email",
+              },
+            })}
+          />
+          <Form.Text className="text-danger">{errors.email?.message}</Form.Text>
         </Form.Group>
         <Form.Group className="mb-3 text-light" controlId="password">
           <Form.Label>Contraseña:*</Form.Label>
-          <Form.Control type="password" placeholder="*********" />
-          <Form.Text className="text-danger">Error</Form.Text>
+          <Form.Control
+            type="password"
+            placeholder="*********"
+            {...register("password", {
+              required: "La contraseña es obligatoria",
+              minLength: {
+                value: 8,
+                message: "La contraseña debe tener como minimo 8 caracteres",
+              },
+              maxLength: {
+                value: 16,
+                message: "La contraseña debe tener 16 caracteres como máximo",
+              },
+              pattern: {
+                value: /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/,
+                message:
+                  "La contraseña debe tener al menos, una máyuscula, una minuscula y un número",
+              },
+            })}
+          />
+          <Form.Text className="text-danger">
+            {errors.password?.message}
+          </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3 text-light" controlId="rol">
           <Form.Label>Rol:*</Form.Label>
@@ -56,7 +114,7 @@ const UserForm = ({ editar, titulo }) => {
           <p>Los campos que tienen * son obligatorios.</p>
         </Form.Group>
         <Form.Group className="mb-3">
-          <Button variant="success me-2">Guardar</Button>
+          <Button type="submit" variant="success me-2">Guardar</Button>
           <Link to="/administrador" className="btn btn-primary">
             Volver
           </Link>
