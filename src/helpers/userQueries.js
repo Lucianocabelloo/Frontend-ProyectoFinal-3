7bc223b4ac8ba1c5b17505de582801b1a5f28c22
nicoIosa <nicolasiosa@hotmail.com>
2024-04-11 15:26:10 -0300

    Se agrego: la funcion readUsersAPI en helpers/userQueries.js

---
+++ src/helpers/userQueries.js
@@ -0,0 +1,13 @@
+const URI_USER = import.meta.env.VITE_API_USER;
+
+export const readUsersAPI = async () => {
+    try {
+      const answer = await fetch(URI_ROOM);
+      return answer;
+    } catch (error) {
+      console.error(
+        `El siguiente error ocurrio al intentar obtener los usuarios: ${error}`
+      );
+    }
+  };
+  
