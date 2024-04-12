d808d9b979d1cae2a6abb96d2d30d15518528606
nicoIosa <nicolasiosa@hotmail.com>
2024-04-11 21:20:29 -0300

    Se agregaron los componentes y estilos de la pagina de error404

--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -1,12 +1,29 @@
 import { Container } from "react-bootstrap";
 import "./error404.css";
 import image404 from "../../assets/img/404.png";
+import { Link } from "react-router-dom";
 
 const Error404 = () => {
   return (
-    <div className="bgLight">
-      <Container className="myMain"></Container>
-    </div>
+    <Link className="myMain" to="/">
+      <Container className="myCont">
+        <img
+          src={image404}
+          alt="Imagen representativa del error 404"
+          className="myImg img-fluid"
+        />
+        <p className="mt-4 text-center">
+          <b>
+            Lo sentimos, pero la dirección web que has ingresado no está
+            disponible.
+          </b>
+        </p>
+        <div className="littleRect"></div>
+        <p className="mt-2 text-center">
+          Haz click en cualquier lado para volver al inicio
+        </p>
+      </Container>
+    </Link>
   );
 };
 
