ec2add7e4b324f8cec27066b18ed4f8e06133ab6
Martín Cristian Cornejo <martincckd130@gmail.com>
2024-04-12 21:11:17 -0300

    Se realizaron algunas correciones en los colores de la pagina principal

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -43,25 +43,25 @@ const Login = () => {
     <div className="bg-login mainContainer">
       <Container>
         <Row className="box-login my-5">
-          <Col lg={6} className="py-5 px-4">
+          <Col lg={6} className="py-5 px-2 px-sm-4">
             <div className="text-center">
               <h1 className="txt-bg-color txt-montserrat fw-bold mb-4 display-5">
                 Iniciar sesión
               </h1>
               <hr />
               <div className="d-flex justify-content-center gap-4 align-items-center">
-                <button className="icon-login">
-                  <i class="bi bi-google"></i>
-                </button>
-                <button className="icon-login">
-                  <i class="bi bi-facebook"></i>
-                </button>
-                <button className="icon-login">
-                  <i class="bi bi-linkedin"></i>
-                </button>
-                <button className="icon-login">
-                  <i class="bi bi-twitter-x"></i>
-                </button>
+                <Link to={'/google'} className="icon-login txt-bg-color">
+                  <i className="bi bi-google"></i>
+                </Link>
+                <Link to={'/facebook'} className="icon-login txt-bg-color">
+                  <i className="bi bi-facebook"></i>
+                </Link>
+                <Link to={'/linkedin'} className="icon-login txt-bg-color">
+                  <i className="bi bi-linkedin"></i>
+                </Link>
+                <Link to={'/twitter-x'} className="icon-login txt-bg-color">
+                  <i className="bi bi-twitter-x"></i>
+                </Link>
               </div>
             </div>
             <Form className="mt-4" onSubmit={handleSubmit(onSubmit)}>
@@ -125,12 +125,12 @@ const Login = () => {
                 </Form.Text>
               </Form.Group>
               <div className="text-center">
-                <a
-                  href="#"
+                <Link
+                  to={'/recuperar-password'}
                   className="text-secondary fw-bold text-center text-decoration-none"
                 >
                   Olvidó su contraseña?
-                </a>
+                </Link>
                 <button
                   type="submit"
                   className="btn-customized-2 d-block m-auto mt-4 fs-5"
