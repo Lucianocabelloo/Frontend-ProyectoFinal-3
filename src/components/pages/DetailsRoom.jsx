63e4dc76ad9042b9cc19cd3e11783d635381a999
Martín Cristian Cornejo <martincckd130@gmail.com>
2024-04-12 22:47:31 -0300

    Finalizado el diseño de la pagina de detalles de habitacion

--- src/components/pages/DetailsRoom.jsx
+++ src/components/pages/DetailsRoom.jsx
@@ -18,7 +18,7 @@ const DetailsRoom = () => {
             />
           </div>
         </Col>
-        <Col lg={6} className="position-relative text-center">
+        <Col lg={6} className="position-relative text-center mt-4 mt-lg-0">
           <span className="room-number">1</span>
           <h2 className="text-uppercase">Nombre de habitación</h2>
           <div className="d-flex align-items-center justify-content-center my-3">
@@ -27,19 +27,29 @@ const DetailsRoom = () => {
             <div className="line"></div>
           </div>
           <Row className="align-items-center justify-content-center mt-4">
-            <Col xs={4} className="txt-details-color">
-              <FontAwesomeIcon icon="fa-solid fa-user" className="fs-4 mb-2"/>
+            <div className="col-4 txt-details-color">
+              <FontAwesomeIcon icon="fa-solid fa-user" className="fs-4 mb-2" />
               <h3 className="fs-5 txt-details-color fw-semibold">Doble</h3>
-            </Col>
-            <Col xs={4} className="txt-details-color">
-              <FontAwesomeIcon icon="fa-solid fa-bed" className="fs-4 mb-2"/>
+            </div>
+            <div className="col-4 txt-details-color">
+              <FontAwesomeIcon icon="fa-solid fa-bed" className="fs-4 mb-2" />
               <h3 className="fs-5 txt-details-color fw-semibold">Deluxe</h3>
-            </Col>
-            <Col xs={12} sm={4} className="txt-details-color mt-3 mt-sm-0">
-              <FontAwesomeIcon icon="fa-solid fa-clock" className="fs-4 mb-2"/>
+            </div>
+            <div className="col-12 col-sm-4 txt-details-color mt-2 mt-sm-0">
+              <FontAwesomeIcon icon="fa-solid fa-clock" className="fs-4 mb-2" />
               <h3 className="fs-5 txt-details-color fw-semibold">Disponible</h3>
-            </Col>
+            </div>
           </Row>
+          <div className="mt-4">
+            <p>
+              La habitación Deluxe ofrece lujo y comodidad. Con cama king-size,
+              baño de mármol, y vistas desde el balcón. Servicios exclusivos
+              como minibar y Wi-Fi. Una experiencia inolvidable de confort y
+              sofisticación.
+            </p>
+            <p className="price">$5000</p>
+            <button className="btn-customized">Reservar habitación</button>
+          </div>
         </Col>
       </Row>
     </Container>
