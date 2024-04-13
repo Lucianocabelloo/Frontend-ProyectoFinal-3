0e93c59ec9527c6575c88ccba3085a8841337aaa
Martín Cristian Cornejo <martincckd130@gmail.com>
2024-04-12 21:41:57 -0300

    Se agrego una imagen ilustrativa de la habitacion

--- src/components/pages/DetailsRoom.jsx
+++ src/components/pages/DetailsRoom.jsx
@@ -1,8 +1,21 @@
+import { Col, Container, Row } from "react-bootstrap";
+
 const DetailsRoom = () => {
     return (
-        <div>
-            
-        </div>
+        <Container className="my-4">
+            <h1 className="kaushan-script fw-semibold">Detalles de <span className="txt-details-color">habitación</span></h1>
+            <hr className="txt-details-color"/>
+            <Row>
+                <Col lg={6}>
+                    <div className="img-container-detail">
+                    <img src="https://images.pexels.com/photos/18368842/pexels-photo-18368842/free-photo-of-hotel-habitacion-pintura-pintando.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="img-room-detail" alt="Imagen de la habitación"/>
+                    </div>
+                </Col>
+                <Col lg={6}>
+
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
