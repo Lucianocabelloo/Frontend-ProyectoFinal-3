8f17de2c434f1a306d161ba01d29d0c2ffd08f6c
Martín Cristian Cornejo <martincckd130@gmail.com>
2024-04-16 02:09:49 -0300

    Se realizaron pequeñas modificariones en la pagina de rooms

--- src/components/pages/rooms/RoomCard.jsx
+++ src/components/pages/rooms/RoomCard.jsx
@@ -3,16 +3,18 @@ import { Link } from "react-router-dom";
 
 const RoomCard = ({room}) => {
   return (
-    <Col md="4" lg="3" className="mb-2">
+    <Col md="4" lg="3" className="mb-2 my-5">
       <Card>
         <Card.Img
           variant="top"
           src={room.imagen}
+          className="card-room-img"
         />
-        <Card.Body className="text-center">
-          <Badge pill className="badgePosition">{room.categoria}</Badge>
-          <Card.Title>Habitación {room.numero}</Card.Title>
+        <Card.Body className="text-center py-4">
+          <Badge pill className="badgePosition mb-3 fs-6">{room.categoria}</Badge>
+          <Card.Title className="fs-3">Habitación {room.numero}</Card.Title>
           <Card.Text>{room.tipoHabitacion}</Card.Text>
+          <p className="fw-semibold txt-details-color fs-4">${room.precio}</p>
           <Link to={'/detalle-habitacion/'+room._id} className="btn btnCardRoom">Ver Habitación</Link>
         </Card.Body>
       </Card>
